4b23aa9b3249d3b2492281231bb5952e817e79ac
marchik88 <marchik88@gmail.com>
2025-01-02 20:47:43 +0200

    Update ui

--- app/Telegram/page.tsx
+++ app/Telegram/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { useState } from "react";
 import Image from "next/image";
 import TelegramLoginButton, { TelegramUser } from 'telegram-login-button'
 
 export default function TelegramAuth() {
-  const [user] = useState({
-    first_name: '',
-    photo_url: ''
-  });
-
 
   return (
     <div
@@ -26,25 +20,10 @@ export default function TelegramAuth() {
         />
         <div className="flex flex-col items-center gap-4">
           <h1 className="text-lg font-semibold">Telegram Authentication</h1>
-          {user ? (
-            <div className="text-center">
-              <h2 className="text-xl">Welcome, {user?.first_name}!</h2>
-              {user?.photo_url && (
-                <Image
-                  src={user?.photo_url}
-                  alt="User Avatar"
-                  width={100}
-                  height={100}
-                  className="rounded-full"
-                />
-              )}
-            </div>
-          ) : (
-            <TelegramLoginButton
-              botName="marchugan_telegram_auth_bot"
-              dataOnauth={(user: TelegramUser) => console.log(user)}
-            />
-          )}
+          <TelegramLoginButton
+            botName="marchugan_telegram_auth_bot"
+            dataOnauth={(user: TelegramUser) => console.log(user)}
+          />
         </div>
       </main>
       <footer
