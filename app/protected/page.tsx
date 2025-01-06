9ceac683d00c22f9f4de12b454c5101e3865335d
marchik88 <marchik88@gmail.com>
2025-01-06 18:29:45 +0200

    update

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/lib/auth";
 
 export default async function ProtectedPage() {
   const session = await getServerSession(authOptions);
